c38b685db0989406d7f826f6454e408356244bdc
Yasin Satar <yasinsatar@hotmail.com>
2023-02-18 23:57:50 +0300

    Revert "Kayıt yapma"
    This reverts commit a03cd4993ee21fe1a4bfaa1164d342c411456241.

--- resources/js/views/auth/register.vue
+++ resources/js/views/auth/register.vue
@@ -1,134 +1,76 @@
-<template>
+<template >
     <div>
-        <form @submit.prevent="submitHandler">
-            <div class="row mb-3">
-                <label for="name" class="col-md-4 col-form-label text-md-end"
-                    >Name</label
-                >
+        <form method="POST" >
+                     
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
 
-                <div class="col-md-6">
-                    <input
-                        v-model="form.name"
-                        id="name"
-                        type="text"
-                        class="form-control"
-                        name="name"
-                        
-                        required
-                        autocomplete="name"
-                        autofocus
-                    />
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control " name="name" value="" required autocomplete="name" autofocus>
 
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ message }}</strong>
-                    </span>
-                </div>
-            </div>
+                           
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ message }}</strong>
+                                    </span>
+                               
+                            </div>
+                        </div>
 
-            <div class="row mb-3">
-                <label for="email" class="col-md-4 col-form-label text-md-end"
-                    >Email</label
-                >
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
 
-                <div class="col-md-6">
-                    <input
-                        v-model="form.email"
-                        id="email"
-                        type="email"
-                        class="form-control"
-                        name="email"
-                       
-                        required
-                        autocomplete="email"
-                    />
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control " name="email" value="" required autocomplete="email">
 
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ message }}</strong>
-                    </span>
-                </div>
-            </div>
+                               
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ message }}</strong>
+                                    </span>
+                               
+                            </div>
+                        </div>
 
-            <div class="row mb-3">
-                <label
-                    for="password"
-                    class="col-md-4 col-form-label text-md-end"
-                    >Password</label
-                >
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">Password</label>
 
-                <div class="col-md-6">
-                    <input
-                        v-model="form.password"
-                        id="password"
-                        type="password"
-                        class="form-control"
-                        name="password"
-                        required
-                        autocomplete="new-password"
-                    />
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control " name="password" required autocomplete="new-password">
 
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ message }}</strong>
-                    </span>
-                </div>
-            </div>
+                               
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ message }}</strong>
+                                    </span>
+                            
+                            </div>
+                        </div>
 
-            <div class="row mb-3">
-                <label
-                    for="password-confirm"
-                    class="col-md-4 col-form-label text-md-end"
-                    >Confirm Password</label
-                >
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">Confirm Password</label>
 
-                <div class="col-md-6">
-                    <input
-                        v-model="form.password_confirmation"
-                        id="password-confirm"
-                        type="password"
-                        class="form-control"
-                        name="password_confirmation"
-                        required
-                        autocomplete="new-password"
-                    />
-                </div>
-            </div>
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
 
-            <div class="row mb-0">
-                <div class="col-md-6 offset-md-4">
-                    <button type="submit" class="btn btn-primary">
-                        Register
-                    </button>
-                </div>
-            </div>
-        </form>
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
     </div>
 </template>
 <script>
-
-
 export default {
-    data() {
-        return {
-            form: {
-                name: "Yasin Satar",
-                email: "yasinsatar@hotmail.com",
-                password: "12345678",
-                password_confirmation: "12345678",
-            },
-            message: "",
-        };
-    },
-    methods: {
-        submitHandler() {
-            axios
-                .post("api/auth/register", { ...this.form })
-                .then((res) => {
-                    console.log(res);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        },
-    },
-};
+    data(){
+        return{
+            message: ""
+        }
+    }
+}
 </script>
-<style></style>
+<style >
+    
+</style>
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,18 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('auth')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
-});
-
-Route::middleware('auth:api')->group(function () {
-    Route::get('userDetail', [AuthController::class, 'user']);
-
-}); 
-
-
-/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
- */
